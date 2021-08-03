c5defb034700f4adde9a522d18ff00b6ab3f9160
Giovanni Brex <Giovanni.Brex@hotmail.com>
2021-08-03 13:43:41 -0500

    Clean up

--- script.js
+++ script.js
@@ -30,22 +30,3 @@ window.addEventListener("load", function () {
     event.preventDefault();
   });
 });
-
-// let usernameInput = document.querySelector("input[name=username]");
-// alert the current value found in the username input
-// alert("username: " + usernameInput.value);
-// const pilot = document.querySelector("input[name=pilotName]");
-// const copilot = document.querySelector("input[name=copilotName]");
-// const fuelLevel = document.querySelector("input[name=fuelLevel]");
-// const cargoMass = document.querySelector("input[name=cargoMass]");
-// if (
-//   !pilot.value ||
-//   !copilot.value ||
-//   !fuelLevel.value ||
-//   !cargoMass.value
-// ) {
-//   if (isNaN(cargoMass.value) || isNaN(fuelLevel.value)) {
-//     alert("Please be sure to fill in fields properly.");
-//   }
-//   alert("Please fill in all fields.");
-// }
